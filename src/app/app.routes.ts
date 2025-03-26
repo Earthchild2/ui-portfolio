00c0137acf84601ea615f6c74da743fc633c1089
Nthabiseng-Fakude <nthabisengfakude.2@gmail.com>
2025-03-26 11:40:40 +0200

    app files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,47 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+export const routes: Routes = [
+    {
+        path: '',   
+        component: HomeComponent
+      },
+      
+      {
+        path: 'projects',  
+        component: ProjectComponent
+      },
+      
+      {
+        path: 'skills',
+        component: SkillsComponent,
+      },
+
+      {
+        path:'contact',
+        component: ContactComponent
+      }
+      
+      // {
+      //   path: '',  
+      //   redirectTo: '',
+      //   pathMatch: 'full'
+      // },
+      // {
+      //   path: '**',  
+      //   redirectTo: ''
+      // }
+    ];
+
+
+    // @NgModule({
+    //     imports: [RouterModule.forRoot(routes)],  
+    //     exports: [RouterModule]
+    //   })
+    //   export class AppRoutingModule { }
+
