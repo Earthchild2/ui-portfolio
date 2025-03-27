002aebe0661d3a8c78a5fc437e8a72f45d60b4d1
Earthchild2 <nthabiseng.fakude2@gmail.com>
2025-03-27 12:26:42 +0200

    made changes on the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -27,21 +27,8 @@ export const routes: Routes = [
         component: ContactComponent
       }
       
-      // {
-      //   path: '',  
-      //   redirectTo: '',
-      //   pathMatch: 'full'
-      // },
-      // {
-      //   path: '**',  
-      //   redirectTo: ''
-      // }
+      
     ];
 
 
-    // @NgModule({
-    //     imports: [RouterModule.forRoot(routes)],  
-    //     exports: [RouterModule]
-    //   })
-    //   export class AppRoutingModule { }
 
